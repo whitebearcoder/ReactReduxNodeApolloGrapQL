648508ce5ee9bb3deff2b041d5ea3cf20ba7202d
whitebearcoder <denisvaran@outlook.com>
2020-06-20 06:28:03 +0200

    mutation: create & update user

--- server/app/GraphQL/users.js
+++ server/app/GraphQL/users.js
@@ -17,6 +17,11 @@ export const typeDefs = gql`
     name: String
     email: String
   }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+    updateUser(userId: ID!, userInput: UserInput): User
+  }
 `;
 
 export const resolvers = {
@@ -25,5 +30,18 @@ export const resolvers = {
       return await db.users.findAll();
     },
     user: async (obj, args, context, info) => db.users.findByPk(args.id),
+  },
+  Mutation: {
+    createUser: async (obj, args, context, info) => {
+      console.log(args);
+      return db.users.create({ ...args });
+    },
+    updateUser: async (obj, args, context, info) => {
+      const user = await db.users.findByPk(args.userId);
+      console.log(user);
+      if (!user) throw new Error(`Couldn’t find user with id ${args.userId}`);
+      await user.set({ ...args.userInput });
+      return user.save();
+    },
   },
 };
